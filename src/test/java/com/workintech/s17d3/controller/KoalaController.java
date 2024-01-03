832b0bee4ef305193ad49434016b14578bb5b90a
Deniz Demir <125642528+deniz-9@users.noreply.github.com>
2024-01-03 21:45:15 +0300

    tamamlandı.

---
+++ src/test/java/com/workintech/s17d3/controller/KoalaController.java
@@ -0,0 +1,5 @@
+package com.workintech.s17d3.controller;
+
+public class KoalaController {
+
+}
---
+++ src/test/java/com/workintech/s17d3/entity/Koala.java
@@ -0,0 +1,15 @@
+package com.workintech.s17d3.entity;
+
+import lombok.Data;
+
+@Data
+public class Koala {
+    private Integer id;
+
+    private String name;
+    private Double sleepHour;
+
+    private Double weight;
+
+    private String gender;
+}
